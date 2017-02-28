24ac779db7cd69bb7a7716068e82fe4af11c4832
Adam Sheeres-Paulicpulle <asheeres-paulicpulle@rsgc.on.ca>
2017-02-28 14:39:27 -0500

    Got initial scene to show. Deleted two unimportant files. The view isn't aligning with the window yet. Soon, I hope to add the menu and then will start adding levels.

--- Pipes/ViewController.swift
+++ Pipes/ViewController.swift
@@ -8,30 +8,21 @@
 
 import Cocoa
 import SpriteKit
-import GameplayKit
 
 class ViewController: NSViewController {
-
-    @IBOutlet var skView: SKView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if let view = self.skView {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        
+        let scene = GameScene(size: CGSize(width: 600, height: 800))
+        
+        let skView = SKView(frame: NSRect(origin: CGPoint(x: 0, y:0), size: CGSize(width: 600, height: 800)))
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        view.addSubview(skView)
+        scene.scaleMode = .aspectFit
+        skView.presentScene(scene)
     }
 }
+
 
