61e77eb85f236da5119f95d7e5fd998ef9abfb2f
Adam Sheeres-Paulicpulle <asheeres-paulicpulle@rsgc.on.ca>
2017-03-02 08:39:21 -0500

    Changed canvas size from 600x800 to 800x600. I thought that this would be more realistic for the app because it is going to be played on a computer, not a mobile device. Also, i've added the media tracking form.

--- Pipes/ViewController.swift
+++ Pipes/ViewController.swift
@@ -13,9 +13,9 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scene = GameScene(size: CGSize(width: 600, height: 800))
+        let scene = GameScene(size: CGSize(width: 800, height: 600))
         
-        let skView = SKView(frame: NSRect(origin: CGPoint(x: 0, y:0), size: CGSize(width: 600, height: 800)))
+        let skView = SKView(frame: NSRect(origin: CGPoint(x: 0, y:0), size: CGSize(width: 800, height: 600)))
         skView.showsFPS = true
         skView.showsNodeCount = true
         skView.ignoresSiblingOrder = true
