cd58b36daa771e076a4823c9065475f4c24fac60
Adam Sheeres-Paulicpulle <asheeres-paulicpulle@rsgc.on.ca>
2017-03-06 13:56:18 -0500

    Added background as well as title for 'pipes.'

--- Pipes/GameScene.swift
+++ Pipes/GameScene.swift
@@ -12,6 +12,28 @@ import GameplayKit
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
+        var sceneNumber = 0
+        
+        //Initial Start Page
+        if sceneNumber == 0 {
+            
+            //Add background image
+            let background = SKSpriteNode(imageNamed: "Background")
+            background.position = CGPoint(x: size.width/2, y: size.height/2)
+            background.size = self.frame.size
+            addChild(background)
+            
+            //Add text for Pipes Title
+            let pipesTitle = SKLabelNode(fontNamed: "Chalkduster")
+            pipesTitle.text = "Start"
+            pipesTitle.fontSize = 64
+            pipesTitle.fontColor = SKColor.black
+            pipesTitle.position = CGPoint(x: size.width/2, y: size.height*0.75)
+            addChild(pipesTitle)
+        }
+        //Level Select
+        if sceneNumber == 2 {
+            
+        }
     }
-
 }
