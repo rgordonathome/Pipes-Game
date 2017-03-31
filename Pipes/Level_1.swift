ff2da749e908cbb342d51b9b2f9a66e843460971
Adam Sheeres-Paulicpulle <asheeres-paulicpulle@rsgc.on.ca>
2017-03-31 13:59:44 -0400

    The grid on page 1 is not proper right now. I need to make sure the positioning is correct, and right now the grid is a 7x7 which is not what I want. I would like it to be a 6x6 grid because I think that is appropriate for the first level. Also, the boxes spawn point is their bottom left corner. I would like to know how to change that to the middle to make things easier for me.

--- Pipes/Level_1.swift
+++ Pipes/Level_1.swift
@@ -12,11 +12,27 @@ import GameplayKit
 class level1: SKScene  {
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
+        var boxCount : Int = 0
         
+        //Background
         let background = SKSpriteNode(imageNamed: "Background3")
         background.position = CGPoint(x: size.width/2, y: size.height/2)
         background.size = self.frame.size
         background.zPosition = 0
         addChild(background)
+        
+        //Add grid
+        for j in stride(from: 488, through: 38, by: -75) {
+            for i in stride(from: 187, through: 637, by: 75) {
+                let boxGrid = SKShapeNode(rect: CGRect(x: i, y: j, width: 75, height: 75))
+                boxGrid.zPosition = 1
+                boxGrid.fillColor = SKColor.clear
+                boxGrid.name = "boxGRid-\(boxCount)"
+                addChild(boxGrid)
+                
+                boxCount += 1
+            }
+        }
+        
     }
 }
