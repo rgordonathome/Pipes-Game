6f44fc69c1e3e942fe00633839b868953e0a5dca
Adam Sheeres-Paulicpulle <asheeres-paulicpulle@rsgc.on.ca>
2017-03-31 14:14:14 -0400

    I've gotten the grid working by manually changing the points; I wasn't able to change the anchor point. I will now proceed to add the colour dots.

--- Pipes/Level_1.swift
+++ Pipes/Level_1.swift
@@ -22,15 +22,20 @@ class level1: SKScene  {
         addChild(background)
         
         //Add grid
-        for j in stride(from: 488, through: 38, by: -75) {
-            for i in stride(from: 187, through: 637, by: 75) {
-                let boxGrid = SKShapeNode(rect: CGRect(x: i, y: j, width: 75, height: 75))
+        for j in stride(from: 425, through: 125, by: -50) {
+            for i in stride(from: 225, through: 525, by: 50) {
+                let boxGrid = SKShapeNode(rect: CGRect(x: i, y: j, width: 50, height: 50))
                 boxGrid.zPosition = 1
                 boxGrid.fillColor = SKColor.clear
                 boxGrid.name = "boxGRid-\(boxCount)"
                 addChild(boxGrid)
                 
                 boxCount += 1
+                
+                let box = SKShapeNode(rect: CGRect(x: self.frame.size.width/2 - 50, y: self.frame.size.height/2 - 50, width: 100, height: 100))
+                box.zPosition = 2
+                box.fillColor = SKColor.black
+                addChild(box)
             }
         }
         
