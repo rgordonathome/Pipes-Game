64c81ce94444c5531ca565c8bba21fc4dab9cb2d
Adam Sheeres-Paulicpulle <asheeres-paulicpulle@rsgc.on.ca>
2017-04-18 09:07:42 -0400

    I will be mapping the value of mouse clicked to a value on the grid from 0-6.

--- Pipes/Level_1.swift
+++ Pipes/Level_1.swift
@@ -13,14 +13,14 @@ class level1: SKScene  {
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
         var boxCount : Int = 0
-        let circlePosiitonArray = [250, 450, 300, 400, 350, 400, 300, 200, 300, 350, 450, 350, 300, 300, 400, 250, 350, 300, 450, 250]
+        let circlePositionArray = [250, 450, 300, 400, 350, 400, 300, 200, 300, 350, 450, 350, 300, 300, 400, 250, 350, 300, 450, 250]
         
         //Background
         let background = SKSpriteNode(imageNamed: "Background3")
         background.position = CGPoint(x: size.width/2, y: size.height/2)
         background.size = self.frame.size
         background.zPosition = 0
-        addChild(background)
+        //addChild(background)
         
         //Title
         let titleText = SKLabelNode(fontNamed: "Helvetica")
@@ -43,10 +43,10 @@ class level1: SKScene  {
                 boxCount += 1
             }
         }
-     //Add the different circles to connect the dots
+        //Add the different circles to connect the dots
         //Red circle 1
         let circleR1 = SKShapeNode(circleOfRadius: 22)
-        circleR1.position = CGPoint(x: circlePosiitonArray[0], y: circlePosiitonArray[1])
+        circleR1.position = CGPoint(x: circlePositionArray[0], y: circlePositionArray[1])
         circleR1.zPosition = 2
         circleR1.strokeColor = SKColor.clear
         circleR1.fillColor = SKColor.red
@@ -55,7 +55,7 @@ class level1: SKScene  {
         
         //Red Circle 2
         let circleR2 = SKShapeNode(circleOfRadius: 22)
-        circleR2.position = CGPoint(x: circlePosiitonArray[2], y: circlePosiitonArray[3])
+        circleR2.position = CGPoint(x: circlePositionArray[2], y: circlePositionArray[3])
         circleR2.zPosition = 2
         circleR2.strokeColor = SKColor.clear
         circleR2.fillColor = SKColor.red
@@ -64,7 +64,7 @@ class level1: SKScene  {
         
         //Blue Circle 1
         let circleB1 = SKShapeNode(circleOfRadius: 22)
-        circleB1.position = CGPoint(x: circlePosiitonArray[4], y: circlePosiitonArray[5])
+        circleB1.position = CGPoint(x: circlePositionArray[4], y: circlePositionArray[5])
         circleB1.zPosition = 2
         circleB1.strokeColor = SKColor.clear
         circleB1.fillColor = SKColor.blue
@@ -73,25 +73,25 @@ class level1: SKScene  {
         
         //Blue Circle 2
         let circleB2 = SKShapeNode(circleOfRadius: 22)
-        circleB2.position = CGPoint(x: circlePosiitonArray[6], y: circlePosiitonArray[7])
+        circleB2.position = CGPoint(x: circlePositionArray[6], y: circlePositionArray[7])
         circleB2.zPosition = 2
         circleB2.strokeColor = SKColor.clear
         circleB2.fillColor = SKColor.blue
         circleB2.name = "circle-4"
         addChild(circleB2)
-
+        
         //Yellow Circle 1
         let circleY1 = SKShapeNode(circleOfRadius: 22)
-        circleY1.position = CGPoint(x: circlePosiitonArray[8], y: circlePosiitonArray[9])
+        circleY1.position = CGPoint(x: circlePositionArray[8], y: circlePositionArray[9])
         circleY1.zPosition = 2
         circleY1.strokeColor = SKColor.clear
         circleY1.fillColor = SKColor.yellow
         circleY1.name = "circle-5"
         addChild(circleY1)
-
+        
         //Yellow Circle 2
         let circleY2 = SKShapeNode(circleOfRadius: 22)
-        circleY2.position = CGPoint(x: circlePosiitonArray[10], y: circlePosiitonArray[11])
+        circleY2.position = CGPoint(x: circlePositionArray[10], y: circlePositionArray[11])
         circleY2.zPosition = 2
         circleY2.strokeColor = SKColor.clear
         circleY2.fillColor = SKColor.yellow
@@ -100,7 +100,7 @@ class level1: SKScene  {
         
         //Purple Circle 1
         let circleP1 = SKShapeNode(circleOfRadius: 22)
-        circleP1.position = CGPoint(x: circlePosiitonArray[12], y: circlePosiitonArray[13])
+        circleP1.position = CGPoint(x: circlePositionArray[12], y: circlePositionArray[13])
         circleP1.zPosition = 2
         circleP1.strokeColor = SKColor.clear
         circleP1.fillColor = SKColor.purple
@@ -109,7 +109,7 @@ class level1: SKScene  {
         
         //Purple Circle 2
         let circleP2 = SKShapeNode(circleOfRadius: 22)
-        circleP2.position = CGPoint(x: circlePosiitonArray[14], y: circlePosiitonArray[15])
+        circleP2.position = CGPoint(x: circlePositionArray[14], y: circlePositionArray[15])
         circleP2.zPosition = 2
         circleP2.strokeColor = SKColor.clear
         circleP2.fillColor = SKColor.purple
@@ -118,7 +118,7 @@ class level1: SKScene  {
         
         //Green Circle 1
         let circleG1 = SKShapeNode(circleOfRadius: 22)
-        circleG1.position = CGPoint(x: circlePosiitonArray[16], y: circlePosiitonArray[17])
+        circleG1.position = CGPoint(x: circlePositionArray[16], y: circlePositionArray[17])
         circleG1.zPosition = 2
         circleG1.strokeColor = SKColor.clear
         circleG1.fillColor = SKColor.green
@@ -127,25 +127,19 @@ class level1: SKScene  {
         
         //Green Circle 2
         let circleG2 = SKShapeNode(circleOfRadius: 22)
-        circleG2.position = CGPoint(x: circlePosiitonArray[18], y: circlePosiitonArray[19])
+        circleG2.position = CGPoint(x: circlePositionArray[18], y: circlePositionArray[19])
         circleG2.zPosition = 2
         circleG2.strokeColor = SKColor.clear
         circleG2.fillColor = SKColor.green
         circleG2.name = "circle-10"
         addChild(circleG2)
     }
-    override func mouseDragged(with event: NSEvent) {
+    override func mouseDown(with event: NSEvent) {
         for node in self.children {
             if let nodeName = node.name {
                 if nodeName.contains("circle") {
                     if node.contains(event.locationInWindow) {
                         let number = nodeName.components(separatedBy: "-")
-                        if String(number[1]) == "1" {
-                            print(number[1])
-                        }
-                        if String(number[1]) == "2" {
-                            print(number[1])
-                        }
                     }
                 }
             }
